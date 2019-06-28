b875324d53fc9a4a9d868cebbdf24c601822f3b7
tdascoli <thomas@dasco.li>
2019-06-28 22:34:33 +0200

    slim

--- mock/api/index.php
+++ mock/api/index.php
@@ -11,5 +11,23 @@ $app->get('/hello/{name}', function (Request $request, Response $response, array
 
     return $response;
 });
+
+$app->post('/testfunct', testFunct);
+function testFunct($tmsg) {
+   $request = Slim::getInstance()->request();
+   echo $request->getBody();
+}
+
+$app->put('/forms/{formId}',  function (Request $request, Response $response, array $args) {
+    $formId = $args['formId'];
+
+    $body = $request->getBody();
+    $user = json_decode($body);
+    //var_dump($user);
+
+    $response->getBody()->write("Hello, $formId:".$user->test);
+
+    return $response;
+});
 $app->run();
 ?>
