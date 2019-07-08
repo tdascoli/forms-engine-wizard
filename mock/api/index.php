2c9b928e4023219e017d6e484fb773ac0c523c8b
tdascoli <thomas@dasco.li>
2019-07-08 23:25:58 +0200

    update

--- mock/api/index.php
+++ mock/api/index.php
@@ -3,6 +3,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+use FormsEngine\Answers\Collection\Collection;
+use FormsEngine\Answers\CompleteHandler\ServerCompleteHandler;
+
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
@@ -46,5 +49,11 @@ $app->get('/forms/{formId}',  function (Request $request, Response $response, ar
     }
     return $newResponse;
 });
+
+// FormsEngine
+$app->put('/record/{formId}', ServerCompleteHandler::class . ':save');
+
+$app->get('/record/{formId}[/{type}]', Collection::class . ':load');
+
 $app->run();
 ?>
